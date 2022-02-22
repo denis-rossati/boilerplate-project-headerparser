e1c937a7088dc03d287bd183aeee8b408f705580
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-02-22 20:33:49 -0300

    Refactor tests

--- tests/integration/serverTest.js
+++ tests/integration/serverTest.js
@@ -1,25 +1,33 @@
 const chai = require('chai');
 
-const should = chai.should();
 const chaiHttp = require('chai-http');
 const chaiMatch = require('chai-match');
 const { describe, it } = require('mocha');
 const server = require('../../server');
 
+const should = chai.should();
+const { expect } = chai;
 chai.use(chaiHttp);
-chai.use(should);
 chai.use(chaiMatch);
 
 describe('/GET /api/whoami', () => {
-  it('should return the IP address', (done) => {
+  it("should return 'OK' status and a object", (done) => {
     chai.request(server)
       .get('/api/whoami')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
+        done();
+      });
+  });
+
+  it('should return the IP address', (done) => {
+    chai.request(server)
+      .get('/api/whoami')
+      .end((err, res) => {
         res.body.should.have.property('ipaddress');
         // ngl, I stole it from here: https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
-        res.body.should.have.property('ipaddress').should.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
+        expect(res.body.ipaddress).should.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|::1|::ffff:127.0.0.1$/);
         done();
       });
   });
